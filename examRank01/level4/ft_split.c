c924fa01bb612ceb3de6002dfb341fe7f1407e75
ElPatatin <cristobalpes1@gmail.com>
2022-07-01 13:19:11 +0200

    Better split for the exam

--- examRank01/level4/ft_split.c
+++ examRank01/level4/ft_split.c
@@ -10,112 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
-// Función donde implementamos la función free para liberar las 2 dimensiones
-// de una array. 
-
-static char	**ft_goingfree(char **ptr, int len)
+int	ft_isSpace(char c)
 {
-	int	i;
-
-	i = 0;
-	while (len > i)
-	{
-		free(ptr[i]);
-		i++;
-	}
-	free(ptr);
-	return (NULL);
+	if (c == ' ' && c == '\t' && c == '\n')
+		return (1);
+	return (0);
 }
 
-// Función que usamos para conocer el tamaño de cada "palabra" que esta delimi-
-// tada por el carácter c.
-
-static int	ft_wcount(char const *s, char c)
+int		count_words(char *str)
 {
-	size_t	i;
-	int		nbr_w;
-
-	i = 0;
-	nbr_w = 0;
-	while (i < ft_strlen(s))
+	int num_words = 0;
+	while (ft_isSpace(*str) == 1)
+		++str;
+	while (*str)
 	{
-		while (s[i] && s[i] == c)
-			++i;
-		while (s[i] && s[i] != c)
-		{
-			if (i == 0 || s[i - 1] == c)
-				++nbr_w;
-			++i;
-		}
+		++num_words;
+		while (*str && ft_isSpace(*str) == 0)
+			++str;
+		while (ft_isSpace(*str) == 1)
+			++str;
 	}
-	return (nbr_w);
+	return (num_words);
 }
 
-// Función que recorre la array de 2 dimensiones para alocar la memoría
-// necesaria mediante el uso de la función malloc. En caso de que falle la
-// alocación de memoria, llamaremos a nuestra implementación de la función
-// free y así vaciar el contenido hasta ese momento alocado.
-
-static char	**ft_mem_alloc(char const *s, char c)
+char	*word_dupe(char *str)
 {
-	char	**sptr;
-	int		i;
-	int		j;
-	int		idx;
-
-	sptr = (char **)malloc(sizeof(char *) * (ft_wcount(s, c) + 1));
-	if (!sptr)
+	int i;
+	int len;
+	char *word;
+	
+	i = 0;
+	len = 0;
+	while (str[len] && ft_isSpace(str[len]) == 0)
+		len++;
+	word = malloc(sizeof(char) * (len + 1));
+	if (!word)
 		return (NULL);
-	i = -1;
-	j = 0;
-	while (++i < ft_wcount(s, c))
+	word[len] = '\0';
+	while (i < len)
 	{
-		idx = 0;
-		while (s[j] == c)
-			j++;
-		while ((s[j] && s[j] != c)
-			&& ((j == 0 && s[j] != c) || (s[j] != c)) && idx++ != -1)
-			j++;
-		sptr[i] = (char *)malloc(sizeof(char) * (idx + 1));
-		if (!sptr[i])
-			return (ft_goingfree(sptr, i));
+		word[i] = str[i];
+		++i;
 	}
-	return (sptr);
+	return (word);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char *str)
 {
-	char	**ptr;
-	int		i;
-	int		j;
-	int		idx;
+	int		num_words;
+	int		word_index;
+	char	**array;
 
-	ptr = ft_mem_alloc((char *)s, c);
-	if (!ptr || !s)
+
+	num_words = count_words(str);
+	word_index = 0;
+	array = (char **)malloc(sizeof(char *) * (num_words + 1));
+	if (!array)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (i < ft_wcount((char *)s, c))
+	array[num_words] = 0;
+	while (ft_isSpace(*str) == 1)
+		++str;
+	while (*str)
 	{
-		idx = 0;
-		while ((char)s[j] == c)
-			j++;
-		if ((j == 0 && (char)s[j] != c) || ((char)s[j] != c && (char)s[j]))
-		{
-			while ((char)s[j] && (char)s[j] != c)
-				ptr[i][idx++] = (char)s[j++];
-			ptr[i][idx] = '\0';
-		}
-		i++;
+		array[word_index] = word_dupe(str);
+		++word_index;
+		while (*str &&  && ft_isSpace(*str) == 0)
+			++str;
+		while (ft_isSpace(*str) == 1)
+			++str;
 	}
-	ptr[i] = NULL;
-	return (ptr);
+	return (array);
 }
-
-/*										*/
-/*	La función split intetará siempre que es posible separar una string	*/
-/*	mediante un separador c que tm es pasado como parámetro. La string	*/
-/*	se guardará en una array de 2 dimensiones como si fuera una lista.	*/
-/*										*/
