3817b4cc19113ae5ec07b82d2817234df3a67292
ElPatatin <cristobalpes1gmail.com>
2022-07-23 14:17:51 +0200

    Arreglado pgcd

--- examRank01/level3/ft_pgcd.c
+++ examRank01/level3/ft_pgcd.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	ft_pgcd(int i, int j)
+void	ft_pgcd(int i, int j)
 {
 	int	tmp;
 
-	tmp = i + 1;
-	while (tmp-- > 0)
-		if ((i % tmp) == 0 && (j % tmp) == 0)
-			return (printf("%d", tmp));
-		
+	if (i <= j)
+		tmp = i;
+	else
+		tmp = j;
+	while (tmp > 0)
+	{
+		if (((i % tmp) == 0) && ((j % tmp) == 0))
+			break ;
+		--tmp;
+	}
+	printf("%d", tmp);
 }
 
 int	main(int argc, char **argv)
